47537607a3ee5f0ed47f273bc1546e2b3567da5d
huyhoa93 <hoanh160993@gmail.com>
2016-08-25 14:49:33 +0700

    5

--- app/code/local/Magestore/Membership/Helper/Email.php
+++ app/code/local/Magestore/Membership/Helper/Email.php
@@ -5,6 +5,7 @@ class Magestore_Membership_Helper_Email extends Mage_Core_Helper_Abstract
 	const XML_PATH_NEW_MEMBER_EMAIL = 'membership/general/new_member_email_template';
     const XML_PATH_NEW_PACKAGE_EMAIL = 'membership/general/new_package_email_template';
 	const XML_PATH_NOTIFY_RENEW_PACKAGE_EMAIL = 'membership/general/notify_renew_package_email_template';
+	const XML_PATH_NOTIFY_AUTO_RENEW_PACKAGE_EMAIL = 'membership/general/notify_auto_renew_package_email_template';
 	
 	public function sendEmailNewPackage($memberPackage){
 		$translate = Mage::getSingleton('core/translate');
@@ -47,6 +48,38 @@ class Magestore_Membership_Helper_Email extends Mage_Core_Helper_Abstract
 		$translate = Mage::getSingleton('core/translate');
         $translate->setTranslateInline(false);
 		$template = Mage::getStoreConfig(self::XML_PATH_NOTIFY_RENEW_PACKAGE_EMAIL);
+		
+		$member = Mage::getModel('membership/member')->load($memberPackage->getMemberId());
+		$package = Mage::getModel('membership/package')->load($memberPackage->getPackageId());
+		$recipient = array(
+                		'email' => $member->getMemberEmail(),
+                		'name'  => $member->getMemberName(),
+            		);
+		
+		$mailTemplate = Mage::getModel('core/email_template');
+		
+		$storeId = Mage::app()->getStore()->getId();
+		$mailTemplate->setDesignConfig(array('area'=>'frontend', 'store'=>$storeId))
+			->sendTransactional(
+				$template,
+				Mage::getStoreConfig(self::XML_PATH_EMAIL_IDENTITY, $storeId),
+				$recipient['email'],
+				$recipient['name'],
+				array(
+					'member'  => $member,
+					'package' => $package,
+					'memberPackage' => $memberPackage
+				)
+			);
+		
+		$translate->setTranslateInline(true);
+		
+		return $memberPackage;
+	}	
+	public function sendEmailNotifyAutoRenewPackage($memberPackage){
+		$translate = Mage::getSingleton('core/translate');
+        $translate->setTranslateInline(false);
+		$template = Mage::getStoreConfig(self::XML_PATH_NOTIFY_AUTO_RENEW_PACKAGE_EMAIL);
 		
 		$member = Mage::getModel('membership/member')->load($memberPackage->getMemberId());
 		$package = Mage::getModel('membership/package')->load($memberPackage->getPackageId());
